6d33fe52558f85811153efb98139d882bd218922
lailaz1 <lailazary@gmail.com>
2019-11-26 12:37:35 +0400

    Updates

---
+++ precache-manifest.ec6853815b96d6144713268537c5b0e8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c7eb879b0c8f65b25a4200604c75db",
+    "url": "/clinicalskillsguide/index.html"
+  },
+  {
+    "revision": "f3aedd668f6772e9c30a",
+    "url": "/clinicalskillsguide/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5c54d6df374986f582a3",
+    "url": "/clinicalskillsguide/static/css/main.1489a05b.chunk.css"
+  },
+  {
+    "revision": "f3aedd668f6772e9c30a",
+    "url": "/clinicalskillsguide/static/js/2.0c59708c.chunk.js"
+  },
+  {
+    "revision": "5c54d6df374986f582a3",
+    "url": "/clinicalskillsguide/static/js/main.a4018062.chunk.js"
+  },
+  {
+    "revision": "f16254d331a90cf7d69f",
+    "url": "/clinicalskillsguide/static/js/runtime-main.ec1224b4.js"
+  },
+  {
+    "revision": "dcdfdd01fb122b8a5054c9c3b088ef98",
+    "url": "/clinicalskillsguide/static/media/ABG.dcdfdd01.jpg"
+  },
+  {
+    "revision": "4ce2f2c191bb7aeb76d670655354d0c5",
+    "url": "/clinicalskillsguide/static/media/ABGsyringe.4ce2f2c1.jpeg"
+  },
+  {
+    "revision": "ac2eb6e9a2d09801c1c3c70a806a7c40",
+    "url": "/clinicalskillsguide/static/media/ABGtechnique.ac2eb6e9.jpeg"
+  },
+  {
+    "revision": "a4bc96cdcb8de0206ee21f66a2be49c4",
+    "url": "/clinicalskillsguide/static/media/AlcoholWipe.a4bc96cd.jpeg"
+  },
+  {
+    "revision": "2e009b2cb6b3c93f3817e4a0043c6dba",
+    "url": "/clinicalskillsguide/static/media/AllEquipment.2e009b2c.jpeg"
+  },
+  {
+    "revision": "19bef8e515826335f5beea17aea2f664",
+    "url": "/clinicalskillsguide/static/media/DisposableKidneyDish.19bef8e5.jpeg"
+  },
+  {
+    "revision": "ba0f5fb946a54df776591c7345896e83",
+    "url": "/clinicalskillsguide/static/media/LocalAnaesthetic.ba0f5fb9.jpeg"
+  },
+  {
+    "revision": "2ddf51bf3948037f8ea098885912f81b",
+    "url": "/clinicalskillsguide/static/media/NasalProngs.2ddf51bf.jpeg"
+  },
+  {
+    "revision": "fab5970f106fc3227ad13a512fc2cf1f",
+    "url": "/clinicalskillsguide/static/media/NonRebreatherMask.fab5970f.jpeg"
+  },
+  {
+    "revision": "b9bd66a3535584131a9d53786b6bfbe8",
+    "url": "/clinicalskillsguide/static/media/NonSterileGloves.b9bd66a3.jpeg"
+  },
+  {
+    "revision": "72da2dfa4f94a39ca71a99ab18a76f04",
+    "url": "/clinicalskillsguide/static/media/OxygenFlowRates.72da2dfa.jpeg"
+  },
+  {
+    "revision": "42331f250fc37b10196987715b2d7b21",
+    "url": "/clinicalskillsguide/static/media/PartialRebreatherMask.42331f25.jpeg"
+  },
+  {
+    "revision": "03b3d79ae66162beba996e2ffb22b707",
+    "url": "/clinicalskillsguide/static/media/SeeAllEquipment.03b3d79a.jpeg"
+  },
+  {
+    "revision": "27e911be1ddbe61bf7ac27899026ffa4",
+    "url": "/clinicalskillsguide/static/media/SharpsBin.27e911be.jpeg"
+  },
+  {
+    "revision": "5ad29877460cad19d586fd979eebe89b",
+    "url": "/clinicalskillsguide/static/media/SimpleFaceMask.5ad29877.jpeg"
+  },
+  {
+    "revision": "1a71233eb504f5c5fea6cdef036f2fb9",
+    "url": "/clinicalskillsguide/static/media/StickyDot.1a71233e.jpeg"
+  },
+  {
+    "revision": "67b070a05695844c53f6cca2bf63fe2f",
+    "url": "/clinicalskillsguide/static/media/VenturiMask.67b070a0.jpeg"
+  },
+  {
+    "revision": "a6dd82be75017b71e932516c55af81c8",
+    "url": "/clinicalskillsguide/static/media/mask.a6dd82be.jpg"
+  }
+]);
